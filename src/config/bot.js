6697b4f66a70d4b191386387fe098fcb52b4052e
98734dn <hotdogsuckdonte@gmail.com>
2026-06-17 22:49:33 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Best in the league",
         // Activity type number (0 = Playing).
         type: 0,
       },
